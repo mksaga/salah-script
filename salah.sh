9c64278e8bca9a5e4445cce9a0fd12dfb1aacff4
Mohamed Aly <mohamed.aly06@gmail.com>
2019-12-07 13:01:12 -0500

    Updates script with starter code

--- salah.sh
+++ salah.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+current_time="$(date '+%H:%M')"
+echo ""
+echo "Welcome!"
+echo "Current Time: $current_time"
+echo ""
+
+# No arguments provided:
+if [ $# == 0 ]; then
+    echo "hi";
+fi
+
+#
